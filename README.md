7b0cd0310559bab2b5e1b964cc7cabc412823387
janaAlmomani <jana2000510@gmail.com>
2023-05-22 13:19:53 +0000

    fix: ix capital-finder

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # capital-finder
+## Done by Jana Almomain
+
+- [This is the Deploy Link](https://capital-finder-ashen.vercel.app/)
+
+
+- [Hello, world!](https://capital-finder-ashen.vercel.app/api)
+
+- [The capital of Jordan is Amman.](https://capital-finder-ashen.vercel.app/api/capital-finder?country=Jordan)
+
+- [The capital of Syria is Damascus.](https://capital-finder-ashen.vercel.app/api/capital-finder?country=Syria)
+
+- [Country parameter is missing.](https://capital-finder-ashen.vercel.app/api/capital-finder)
+
+- [The country of Amman is Jordan.](https://capital-finder-ashen.vercel.app/api/country-finder?capital=Amman)
+
--- api/country-finder.py
+++ api/country-finder.py
@@ -10,19 +10,18 @@ class handler(BaseHTTPRequestHandler):
         url_components = parse.urlsplit(s)
         query_strings_list = parse.parse_qsl(url_components.query)
         dic = dict(query_strings_list)
-        country = dic.get("country")
-        print(country)
+        country = dic.get("capital")
+        print(capital)
 
         if country:
-            url = f"https://restcountries.com/v3.1/name/{country}"
+            url = f"https://restcountries.com/v3.1/name/{capital}"
             print(url)
             res = requests.get(url)
-            print(res)
             data = res.json()
-            capital = data[0]["capital"][0]
-            response = f"The capital of {country} is {capital}."
+            capital = data[0]["name"]["common"]
+            response = f"The country of {capital} is {country}."
         else:
-            response = "Country parameter is missing."
+            response = "capital parameter is missing."
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
