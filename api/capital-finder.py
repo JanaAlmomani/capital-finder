1a1e84ccb035bd53228aba9fc509a1bdf08bafc5
janaAlmomani <jana2000510@gmail.com>
2023-05-22 14:51:06 +0000

    Lab16 is done

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -10,19 +10,27 @@ class handler(BaseHTTPRequestHandler):
         url_components = parse.urlsplit(s)
         query_strings_list = parse.parse_qsl(url_components.query)
         dic = dict(query_strings_list)
+
         country = dic.get("country")
-        print(country)
+        capital = dic.get("capital")
+
 
         if country:
             url = f"https://restcountries.com/v3.1/name/{country}"
-            print(url)
             res = requests.get(url)
-            print(res)
             data = res.json()
-            capital = data[0]["capital"][0]
-            response = f"The capital of {country} is {capital}."
+            capitall = data[0]["capital"][0]
+            response = f"The capital of {country} is {capitall}."
+
+        elif capital:
+            url = f"https://restcountries.com/v3.1/capital/{capital}"
+            res = requests.get(url)
+            data = res.json()
+            countryy = data[0]["name"]["common"]
+            response = f"The country of {capital} is {countryy}."
         else:
-            response = "Country parameter is missing."
+            response = "The parameter is missing."
+         
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
